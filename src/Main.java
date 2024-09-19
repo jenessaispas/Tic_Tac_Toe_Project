cd604154ab56cbd6852c932fdec8cfd1e7a8640c
JeNesSaisPas <jenessaispasouplus@gmail.com>
2024-09-19 08:50:52 +0200

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 //Deklaration der Importe
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    //Declaration of static variables for the complete Programm.
+    //Declaration all static variables for the complete Programm.
     static char[][] playground = new char[3][3];
     static char playerSymbol;
     static int count = 0;
     static String txtFilled = "This field is already occupied, select one of the free fields, ";
-    static String txtWin = "Congratulations you won ";
+    static String txtWin = "Congratulations you won: ";
     static String playerNameX;
     static String playerNameO;
-    static String txtWrongInput = "This is an invalid input. Please enter only one of the numbers available to choose from \" +\n" +
-            "            \"on the field!";
+    static String txtWrongInput = "This is an invalid input. Please enter only one of the numbers available to choose from the field!";
 
     // Declaration and formulate each own written Methods:
     //Method to print the playground on terminal screen.
@@ -32,9 +32,9 @@ public class Main {
         //Begin of the loop to compare the char inside the fields.
         while (true) {
             if (playerSymbol == 'X') {
-                System.out.println("Please "+playerNameX+" enter a number where you would like to place your pawn:");
+                System.out.println(playerNameX + ", enter a number where you would like to place your 'X' and confirm with enter:");
             } else {
-                System.out.println("Please "+playerNameO+" enter a number where you would like to place your pawn:");
+                System.out.println(playerNameO + ", enter a number where you would like to place your 'O' and confirm with enter:");
             }
             //Runs the Code to get a Userinput in a special Try-Catch Block to catch an exception if the scanner get an wrong input.
             int position;
@@ -50,84 +50,13 @@ public class Main {
             if (position < 1 || position > 9) {
                 continue;
             }
-            //Occupying a field with a player figure
-            switch (position) {
-                case 1:
-                    // This checks whether the field has not already been occupied by a player.
-                    if (playground[0][0] == 'X' || playground[0][0] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[0][0] = playerSymbol;
-                    }
-                    break;
-                case 2:
-                    if (playground[0][1] == 'X' || playground[0][1] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[0][1] = playerSymbol;
-                    }
-                    break;
-                case 3:
-                    if (playground[0][2] == 'X' || playground[0][2] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[0][2] = playerSymbol;
-                    }
-                    break;
-                case 4:
-                    if (playground[1][0] == 'X' || playground[1][0] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[1][0] = playerSymbol;
-                    }
-                    break;
-                case 5:
-                    if (playground[1][1] == 'X' || playground[1][1] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[1][1] = playerSymbol;
-                    }
-                    break;
-                case 6:
-                    if (playground[1][2] == 'X' || playground[1][2] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[1][2] = playerSymbol;
-                    }
-                    break;
-                case 7:
-                    if (playground[2][0] == 'X' || playground[2][0] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[2][0] = playerSymbol;
-                    }
-                    break;
-                case 8:
-                    if (playground[2][1] == 'X' || playground[2][1] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[2][1] = playerSymbol;
-                    }
-                    break;
-                case 9:
-                    if (playground[2][2] == 'X' || playground[2][2] == 'O') {
-                        System.out.println(txtFilled);
-                        continue;
-                    } else {
-                        playground[2][2] = playerSymbol;
-                    }
-                    break;
-                default:
-                    System.out.println(txtWrongInput);
-                    continue;
+            //Checking if the position is already placed whit a playersymbol
+            if (playground[position * (playground.length) / 10][(position - 1) % 3] == 'X' || playground[position * (playground.length) / 10][(position - 1) % 3] == 'O') {
+                System.out.println(txtFilled);
+                continue;
+                //Occupying a field with a playersymbol
+            } else {
+                playground[position * (playground.length) / 10][(position - 1) % 3] = playerSymbol;
             }
             printPlayground(playground);
             break;
@@ -167,9 +96,9 @@ public class Main {
 
     public static void main(String[] args) {
         //The program begins with a request for players to state their names.
-        System.out.println("Please Player 1 enter your name:");
+        System.out.println("Please Player 1 choose your name and confirm with enter:");
         playerNameX = new Scanner(System.in).nextLine();
-        System.out.println("Now it's player 2's turn to enter his name:");
+        System.out.println("Now it's player 2's turn to choose his name and to confirm with enter:");
         playerNameO = new Scanner(System.in).nextLine();
         char fieldCounter = '1';
         //Initialization and assignment of the playing field with characters from 1 - 9.
@@ -182,11 +111,11 @@ public class Main {
         printPlayground(playground);
         //Gameplay within the maximum number of rounds.
         while (count < 9) {
-            if (youWin(playerSymbol, playground) && playerSymbol ==  'X') {
-                System.out.println(txtWin+playerNameX+" !!!");
+            if (youWin(playerSymbol, playground) && playerSymbol == 'X') {
+                System.out.println(txtWin + playerNameX + " !!!");
                 break;
-            }else if (youWin(playerSymbol, playground) && playerSymbol == 'O') {
-                System.out.println(txtWin+playerNameO+" !!!");
+            } else if (youWin(playerSymbol, playground) && playerSymbol == 'O') {
+                System.out.println(txtWin + playerNameO + " !!!");
                 break;
             }
             //Selecting which player's turn it is to make their move.
@@ -199,7 +128,7 @@ public class Main {
             }
             count++;
         }
-        if (youWin(playerSymbol,playground) == false && count == 9) {
+        if (!youWin(playerSymbol, playground) && count == 9) {
             System.out.println("Draw!");
         }
     }
